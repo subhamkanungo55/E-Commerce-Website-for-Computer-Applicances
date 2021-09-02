18f590a523835722fc287c3437151801687cd8ee
subhamkanungo55 <79400082+subhamkanungo55@users.noreply.github.com>
2021-09-03 00:45:41 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import Database_connection
+# import Database_connection
 # Create your views here.
 def index(request):
     return render(request,'index.html')
